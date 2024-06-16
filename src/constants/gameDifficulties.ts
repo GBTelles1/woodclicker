8a898023a0b311d06a87e6f0620c56e5cbe33dfe
GBTelles1 <gbertelles@gmail.com>
2024-06-16 15:29:14 -0300

    Create the project constants
    The constans directory has the alphabet, that will be used to generate the letter sequence, and the gameDifficulties, that defines the timer and the points multiplier for each difficulty level.

---
+++ src/constants/gameDifficulties.ts
@@ -0,0 +1,14 @@
+export const gameDifficulties = {
+  Easy: {
+    timeLeft: 10,
+    pointsMultiplier: 1
+  },
+  Medium: {
+    timeLeft: 8,
+    pointsMultiplier: 2
+  },
+  Hard: {
+    timeLeft: 6,
+    pointsMultiplier: 3
+  }
+};
